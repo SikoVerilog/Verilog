6a3624c527c995f940f35bbc5dab89b2e77abbf1
sana899 <43127720+sana899@users.noreply.github.com>
2019-10-22 22:46:21 +0500

    .V file created

---
+++ Assign_4_1_LFSR_external/Assign_4_1_LFSR_external.srcs/sources_1/new/LFSR.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/22/2019 10:45:38 PM
+// Design Name: 
+// Module Name: LFSR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module LFSR(
+    input CLK,
+    input Reset,
+    output [7:0] Y
+    );
+endmodule
