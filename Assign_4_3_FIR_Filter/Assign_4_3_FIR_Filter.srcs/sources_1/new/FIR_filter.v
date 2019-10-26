003fcd7f1f611541169f40e93a237c558af7bfb1
Siko <43127720+sana899@users.noreply.github.com>
2019-10-26 13:19:43 +0500

    Commented

--- Assign_4_3_FIR_Filter/Assign_4_3_FIR_Filter.srcs/sources_1/new/FIR_filter.v
+++ Assign_4_3_FIR_Filter/Assign_4_3_FIR_Filter.srcs/sources_1/new/FIR_filter.v
@@ -21,30 +21,31 @@
 
 
 module FIR_filter(
-    input [15:0] X,
+    input [15:0] X,//input
     input [15:0] S,
     input [15:0] T,
     input [15:0] U,
     input [15:0] V,
-    input [15:0] W,
-    input CLK,
-    input Reset,
-    output reg [15:0] Y
+    input [15:0] W,//cofficent
+    input CLK,//Clock
+    input Reset,//Reset
+    output reg [15:0] Y//Output
     );
-    reg [15:0] Xn [0:3];
+    reg [15:0] Xn [0:3]; //make shift register
     integer i;
-    always @ (posedge CLK)begin
-        if(Reset) begin
+    always @ (posedge CLK)begin//make clock
+        if(Reset) begin//define the reset value
             for(i=0; i<4; i=i+1)begin
-                Xn[i] = 'b0;
+                Xn[i] = 'b0;//default value
             end
         end
         else begin
-            Xn[0] <= X;
+            Xn[0] <= X;//shifting
             for(i=1; i<4; i=i+1)begin
                 Xn[i] <= Xn[i-1];
             end
             Y = X*S + Xn[0]*T + Xn[1]*U + Xn[2]*V + Xn[3]*W;
+			//multiplying cofficent to register value to make output
         end
     end
 endmodule
